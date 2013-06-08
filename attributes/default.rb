17b1b81f18ea4c923b6253156a65aa551cb9403d
Dave Newman <dave@snappyco.de>
2013-06-08 16:20:05 -0700

    install from source

---
+++ attributes/default.rb
@@ -0,0 +1,1 @@
+default['overviewer']['rev'] = 'master'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,19 @@
-apt_repository "overviewer" do
-  uri "http://overviewer.org/debian"
-  distribution './'
+include_recipe 'git'
+include_recipe 'python::pip'
+
+package 'python-dev'
+package 'python-imaging'
+python_pip 'numpy'
+
+git "/opt/overviewer" do
+  repository 'git://github.com/overviewer/Minecraft-Overviewer.git'
+  reference node['overviewer']['rev']
 end
 
-package "minecraft-overviewer" do
-  options "--force-yes"
+bash "build overviewer" do
+  cwd "/opt/overviewer"
+  code 'python setup.py build'
+end
+link "/usr/local/bin/overviewer.py" do
+  to "/opt/overviewer/overviewer.py"
 end
